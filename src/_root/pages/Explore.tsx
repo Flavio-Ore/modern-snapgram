9ac79bfb0864d1158c18ede848ed55d33631a45f
Flavio-Ore <pseudominio.go@gmail.com>
2024-03-18 21:03:34 -0500

    feat: InfiniteScroll -> ExplorerPosts, SearchResults and one api function to call infinite posts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,46 +1,35 @@
-import InfinitePosts from '@/components/shared/InfinitePosts'
-import Loader from '@/components/shared/Loader'
+import ExploreDefaultPosts from '@/components/shared/ExplorePosts'
+import SearchResults from '@/components/shared/SearchResults'
 import { Input } from '@/components/ui/input'
 import { useDebounce } from '@/hooks/useDebounce'
-import { useGetPosts, useSearchPosts } from '@/lib/queries/queriesAndMutations'
+import {
+  useGetPosts,
+  useInfiniteSearchPosts
+} from '@/lib/queries/queriesAndMutations'
 import { useEffect, useState } from 'react'
-import { useInView } from 'react-intersection-observer'
 
 const Explore = () => {
-  const { ref, inView } = useInView()
   const [isSearchLoading, setIsSearchLoading] = useState(false)
-  const {
-    data: infinitePosts,
-    fetchNextPage,
-    isFetching: isInfiniteFetching,
-    hasNextPage
-  } = useGetPosts()
   const [searchValue, setSearchValue] = useState('')
+  const { data: infinitePosts, isFetching: isInfinitePostsFetching } =
+    useGetPosts()
   const debouncedValue = useDebounce(searchValue, 500)
-  const { data: searchedPosts, isFetching: isSearchedFetching } =
-    useSearchPosts({
-      searchTerm: debouncedValue
-    })
+  const {
+    data: infiniteSearchedPosts,
+    isFetching: isInfiniteSearchedFetching
+  } = useInfiniteSearchPosts({
+    searchTerm: debouncedValue
+  })
 
   useEffect(() => {
-    if (inView && !searchValue && !isInfiniteFetching) fetchNextPage()
-  }, [inView, searchValue])
-
-  useEffect(() => {
-    if (!isSearchedFetching) setIsSearchLoading(false)
-  }, [isSearchedFetching])
-
-  if (!infinitePosts)
-    return (
-      <div className='flex-center w-full h-full'>
-        <Loader />
-      </div>
-    )
+    if (!isInfiniteSearchedFetching || !isInfinitePostsFetching)
+      setIsSearchLoading(false)
+  }, [isInfiniteSearchedFetching, isInfinitePostsFetching])
 
   const shouldShowSearchResults = searchValue !== ''
   const shouldShowPosts =
-    !shouldShowSearchResults &&
-    infinitePosts.pages.every(item => item.documents.length === 0)
+    infiniteSearchedPosts?.pages.every(item => item.documents.length === 0) &&
+    infinitePosts?.pages.every(item => item.documents.length === 0)
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
@@ -68,7 +57,41 @@ const Explore = () => {
           />
         </div>
       </div>
-      <InfinitePosts todo={'tasks'} />
+      <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
+        <h3 className='body-bold md:h3-bold'>Popular today</h3>
+        <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
+          <p className='small-medium md:base-medium text-light-2'>All</p>
+          <img
+            src='/assets/icons/filter.svg'
+            width={20}
+            height={20}
+            alt='Filter Icon'
+          />
+        </div>
+      </div>
+      <div className='flex flex-wrap gap-9 w-full max-w-5xl'>
+        {shouldShowSearchResults && (
+          <SearchResults
+            searchValue={debouncedValue}
+            isSearching={isSearchLoading}
+          />
+        )}
+        {!shouldShowSearchResults && (
+          <ExploreDefaultPosts
+            searchValue={debouncedValue}
+            isSearching={isSearchLoading}
+          />
+        )}
+        {shouldShowPosts && (
+          <p className='text-light-4 mt-10 text-center w-full'>End of posts</p>
+        )}
+      </div>
+      {/* 
+      {hasNextPagePosts && !searchValue && (
+        <div ref={ref} className='mt-10'>
+          <Loader />
+        </div>
+      )} */}
     </div>
   )
 }
