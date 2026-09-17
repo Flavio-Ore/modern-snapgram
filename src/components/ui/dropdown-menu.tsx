d734960199a6dcbfd2964861c573a46f95f4503d
Flavio Ore <101116042+Flavio-Ore@users.noreply.github.com>
2026-09-17 10:02:46 -0500

    refactor: remove 'use client' directive from multiple components and optimize imports

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,11 +1,12 @@
-'use client'
-
-
+import { cn } from '@/lib/utils'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { Check, ChevronRightIcon, Circle } from 'lucide-react'
-import * as React from 'react'
-
-import { cn } from '@/lib/utils'
+import {
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+  forwardRef,
+  type HTMLAttributes
+} from 'react'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
@@ -19,9 +20,9 @@ const DropdownMenuSub = DropdownMenuPrimitive.Sub
 
 const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup
 
-const DropdownMenuSubTrigger = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.SubTrigger>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubTrigger> & {
+const DropdownMenuSubTrigger = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.SubTrigger>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubTrigger> & {
   inset?: boolean
 }
 >(({ className, inset, children, ...props }, ref) => (
@@ -41,9 +42,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubTrigger> & {
 DropdownMenuSubTrigger.displayName =
   DropdownMenuPrimitive.SubTrigger.displayName
 
-const DropdownMenuSubContent = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.SubContent>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent>
+const DropdownMenuSubContent = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.SubContent>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent>
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.SubContent
     ref={ref}
@@ -57,9 +58,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent>
 DropdownMenuSubContent.displayName =
   DropdownMenuPrimitive.SubContent.displayName
 
-const DropdownMenuContent = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.Content>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+const DropdownMenuContent = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.Content>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
   <DropdownMenuPrimitive.Portal>
     <DropdownMenuPrimitive.Content
@@ -75,9 +76,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
 ))
 DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
 
-const DropdownMenuItem = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.Item>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
+const DropdownMenuItem = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.Item>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
   inset?: boolean
 }
 >(({ className, inset, ...props }, ref) => (
@@ -93,9 +94,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
 ))
 DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
 
-const DropdownMenuCheckboxItem = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.CheckboxItem>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
+const DropdownMenuCheckboxItem = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.CheckboxItem>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
 >(({ className, children, checked, ...props }, ref) => (
   <DropdownMenuPrimitive.CheckboxItem
     ref={ref}
@@ -117,9 +118,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
 DropdownMenuCheckboxItem.displayName =
   DropdownMenuPrimitive.CheckboxItem.displayName
 
-const DropdownMenuRadioItem = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.RadioItem>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.RadioItem>
+const DropdownMenuRadioItem = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.RadioItem>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.RadioItem>
 >(({ className, children, ...props }, ref) => (
   <DropdownMenuPrimitive.RadioItem
     ref={ref}
@@ -139,9 +140,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.RadioItem>
 ))
 DropdownMenuRadioItem.displayName = DropdownMenuPrimitive.RadioItem.displayName
 
-const DropdownMenuLabel = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.Label>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
+const DropdownMenuLabel = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.Label>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
   inset?: boolean
 }
 >(({ className, inset, ...props }, ref) => (
@@ -157,9 +158,9 @@ React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
 ))
 DropdownMenuLabel.displayName = DropdownMenuPrimitive.Label.displayName
 
-const DropdownMenuSeparator = React.forwardRef<
-React.ElementRef<typeof DropdownMenuPrimitive.Separator>,
-React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
+const DropdownMenuSeparator = forwardRef<
+ElementRef<typeof DropdownMenuPrimitive.Separator>,
+ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
@@ -172,10 +173,13 @@ DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName
 const DropdownMenuShortcut = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLSpanElement>) => {
+}: HTMLAttributes<HTMLSpanElement>) => {
   return (
     <span
-      className={cn('ml-auto subtle-semibold tracking-widest opacity-60', className)}
+      className={cn(
+        'ml-auto subtle-semibold tracking-widest opacity-60',
+        className
+      )}
       {...props}
     />
   )
--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -1,20 +1,17 @@
-'use client'
-
 // Inspired by react-hot-toast library
-import * as React from 'react'
-
 import type {
   ToastActionElement,
   ToastProps
 } from '@/components/ui/toast'
+import { useEffect, useState, type ReactNode } from 'react'
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1_000_000
 
 type ToasterToast = ToastProps & {
   id: string
-  title?: React.ReactNode
-  description?: React.ReactNode
+  title?: ReactNode
+  description?: ReactNode
   action?: ToastActionElement
 }
 
@@ -173,9 +170,9 @@ function toast ({ ...props }: Toast) {
 }
 
 function useToast () {
-  const [state, setState] = React.useState<State>(memoryState)
+  const [state, setState] = useState<State>(memoryState)
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState)
     return () => {
       const index = listeners.indexOf(setState)
