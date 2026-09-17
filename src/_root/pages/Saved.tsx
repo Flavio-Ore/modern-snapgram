22a23654db46c7505c9cd16e30ff27705f1176ce
Flavio Ore <101116042+Flavio-Ore@users.noreply.github.com>
2026-09-17 10:02:13 -0500

    pef(app): lazy loading

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,52 +1,5 @@
 import CustomTabs from '@/components/shared/app/CustomTabs'
-import InfiniteScroll from '@/components/shared/app/InfiniteScroll'
-import Loader from '@/components/shared/app/Loader'
-import GridPostList from '@/components/shared/posts/GridPostList'
-import { useUserContext } from '@/context/useUserContext'
-import { useGetInfiniteSavedPosts } from '@/lib/queries/infiniteQueries'
 import { SAVES_TRIGGERS } from '@/values'
-import { useMemo } from 'react'
-
-export const SavedPosts = () => {
-  const { user } = useUserContext()
-  const { data, isError, isLoading, isFetching, hasNextPage, fetchNextPage } =
-    useGetInfiniteSavedPosts({ userId: user.id })
-  const posts = useMemo(
-    () =>
-      data?.pages.flatMap(savesPage =>
-        savesPage.flatMap(saved => saved.post)
-      ) ?? [],
-    [data]
-  )
-  if (!user)
-    return (
-      <div className='flex-center w-full h-full'>
-        <Loader />
-      </div>
-    )
-  return (
-    <InfiniteScroll
-      data={data}
-      isLoading={isLoading}
-      isError={isError}
-      isDataEmpty={posts.length === 0}
-      fetchNextPage={fetchNextPage}
-      hasNextPage={hasNextPage}
-      isFetching={isFetching}
-    >
-      <GridPostList
-        key={`${posts}-SAVED_REELS-%&`}
-        posts={posts}
-        showStats={false}
-        showUser={false}
-      />
-    </InfiniteScroll>
-  )
-}
-export const SavedReels = () => <h1 className='h1-bold w-full'>Saved Reels</h1>
-export const SavedCollections = () => (
-  <h1 className='h1-bold'>Saved Collections</h1>
-)
 
 const Saved = () => {
   return (
---
+++ src/components/shared/saves/SavedCollections.tsx
@@ -0,0 +1,2 @@
+const SavedCollections = () => <h1 className='h1-bold'>Saved Collections</h1>
+export default SavedCollections
