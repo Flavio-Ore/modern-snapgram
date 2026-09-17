1664aac2f9d4ca3b00eab72b2ac3ecbf8c06d802
Flavio Ore <101116042+Flavio-Ore@users.noreply.github.com>
2026-09-17 10:02:06 -0500

    todo: InfinitePosts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,6 +1,5 @@
-import GridPostList from '@/components/shared/GridPostList'
+import InfinitePosts from '@/components/shared/InfinitePosts'
 import Loader from '@/components/shared/Loader'
-import SearchResults from '@/components/shared/SearchResults'
 import { Input } from '@/components/ui/input'
 import { useDebounce } from '@/hooks/useDebounce'
 import { useGetPosts, useSearchPosts } from '@/lib/queries/queriesAndMutations'
@@ -69,41 +68,7 @@ const Explore = () => {
           />
         </div>
       </div>
-      <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
-        <h3 className='body-bold md:h3-bold'>Popular today</h3>
-        <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
-          <p className='small-medium md:base-medium text-light-2'>All</p>
-          <img
-            src='/assets/icons/filter.svg'
-            width={20}
-            height={20}
-            alt='Filter Icon'
-          />
-        </div>
-      </div>
-      <div className='flex flex-wrap gap-9 w-full max-w-5xl'>
-        {shouldShowSearchResults ? (
-          <SearchResults
-            isSearching={isSearchLoading}
-            searchedPosts={searchedPosts?.documents}
-          />
-        ) : shouldShowPosts ? (
-          <p className='text-light-4 mt-10 text-center w-full'>End of posts</p>
-        ) : (
-          infinitePosts.pages.map((currentPosts, index) => (
-            <GridPostList
-              key={`${currentPosts}-${index}`}
-              posts={currentPosts.documents}
-            />
-          ))
-        )}
-      </div>
-
-      {hasNextPage && !searchValue && (
-        <div ref={ref} className='mt-10'>
-          <Loader />
-        </div>
-      )}
+      <InfinitePosts todo={'tasks'} />
     </div>
   )
 }
--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,5 +1,29 @@
+import { useUserContext } from '@/context/useUserContext'
+
 const Profile = () => {
-  return <div>Profile</div>
+  const { user } = useUserContext()
+  console.log('user :>> ', user)
+  return (
+    <div className='profile-container'>
+      <ul className='profile-inner_container'>
+        <li>
+          <img src={user?.imageUrl} alt={user?.name} className='' />
+        </li>
+        <li>
+          <h1 className='h1-bold'>{user?.name}</h1>
+        </li>
+        <li>
+          <h2 className='h2-bold'>{user?.username}</h2>
+        </li>
+        <li>
+          <h2 className='base-medium'>{user?.email}</h2>
+        </li>
+        <li>
+          <h3>{user?.bio}</h3>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Profile
